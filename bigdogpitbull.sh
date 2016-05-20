377aa9c73928629a63f4b00008f9f46fef390d43
gillesdoutweeb <jsylvia92@gmail.com>
2016-05-19 17:18:43 -0700

    add main menu

--- bigdogpitbull.sh
+++ bigdogpitbull.sh
@@ -1,7 +1,39 @@
 #!/bin/sh
-read -p "How many recent videos do you wish to download? " qty
-if [ "$qty" -gt 0 ]; then
-	./.giant_bomb_cli.py -l $qty --quality hd --download
-else
-	echo "Oh, okay. Aborting."
-fi
+
+Menu () {
+   echo "What would you like to do?"
+   echo "   1. Download recent videos"
+   echo "   2. Search for videos by name"
+   echo "   3. Quit"
+   while read -p "Enter your choice: " mainMenu; do
+      case $mainMenu in
+         1) Download
+         exit
+         ;;
+         2) Search
+         exit
+         ;;
+         3) exit
+         ;;
+         *) echo "Oops, try again.";;
+      esac
+   done
+}
+
+Download () {
+   read -p "How many recent videos do you wish to download? " qty
+   if [ "$qty" -gt 0 ]; then
+      ./.giant_bomb_cli.py -l $qty --quality hd --download
+   else
+      echo "Aborted. Returning to main menu.\n"
+      Menu
+   fi
+
+}
+
+Search () {
+   echo "Ha Ha\n"
+   Menu
+}
+
+Menu
